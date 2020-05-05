37f81e5df612dd95d5ff57907ad288923507ff28
Gao Ciaorun <itchynozz@gmail.com>
2020-05-05 18:20:33 +0800

    jQueryRoR

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,6 +30,7 @@ class ProductsController < ApplicationController
       if @product.save
         format.html { redirect_to products_path, notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
+        
       else
         format.html { render :new }
         format.json { render json: @product.errors, status: :unprocessable_entity }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,53 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
 //bootstrap
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+//select2
+import 'select2'
+import 'select2/dist/css/select2.css'
+
+import $ from 'jquery'
+window.$ = window.jQuery = $
+// window.$ = $
+
+//sweetalert2
+import Swal from 'sweetalert2'
+// import 'sweetalert2/dist/sweetalert2.all.min.js'
+window.Swal = Swal
+import 'sweetalert2'
+// import 'sweetalert2/dist/sweetalert2.min.css'
+$(document).on("turbolinks:load", function(){
+  $('.js-submit-btn').click(function(e){
+    e.preventDefault()
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You will create a product!?",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire({
+            showConfirmButton: false,
+            title: 'Loading...'
+          },
+        )
+        $('.js-form').submit()
+      }
+    })
+  })
+})
+
+
+
+
+
+//datetimepicker
+import 'tempusdominus-bootstrap-4'
+import 'tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.css'
+
+// Fontawesome
+// import 'font-awesome'
